a8e0642ef33cdefd154fc8c48fd46a5dd5bd7765
prince7858 <91559607+prince7858@users.noreply.github.com>
2024-08-31 22:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+	
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    
+    const urlElement = document.getElementById("url");
+    
+    urlElement.textContent = `https://localhost:8080/${name}/${year}`;
+});
